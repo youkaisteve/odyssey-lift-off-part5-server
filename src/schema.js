9b60c0c817f46b15a1bba07185a5e5a38248c78d
steve.k.you <steve.k.you@yzw.cn>
2021-10-11 17:48:59 +0800

    refactor: 💡 fix

--- src/schema.js
+++ src/schema.js
@@ -43,6 +43,8 @@ const typeDefs = gql`
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
+    "The track's full duration, in seconds"
+    durationInSeconds: Int
     "The track's complete array of Modules"
     modules: [Module!]!
   }
@@ -67,6 +69,8 @@ const typeDefs = gql`
     content: String
     "The module's video url, for video-based modules"
     videoUrl: String
+    "The module's video duration, in seconds"
+    durationInSeconds: Int
   }
 `;
 
